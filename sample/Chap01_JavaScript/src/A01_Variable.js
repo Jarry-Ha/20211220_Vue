20142ea91c8ee5772bb63072338e3d37dd983d45
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-20 16:02:38 +0900

    .

---
+++ sample/Chap01_JavaScript/src/A01_Variable.js
@@ -0,0 +1,7 @@
+console.log('name: ' + name + ' typeof: ' + typeof name);
+
+var name = 'NolBu';
+console.log('name: ' + name + ' typeof: ' + typeof name);
+
+var name = 10;
+console.log('name: ' + name + ' typeof: ' + typeof name);
