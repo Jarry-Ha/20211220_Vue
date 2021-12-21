1fde9feaef9216c1a79cd333d4c0e5561b0e5dac
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-21 14:50:19 +0900

    .

---
+++ Chap02_Vue/js/obj.js
@@ -0,0 +1,18 @@
+
+var obj = {
+    name: 'NolBu',
+    age: 30,
+    'address': 'Seoul',
+    100: '010-1234-5678'
+};
+console.log(obj);
+console.log(obj.name);
+console.log(obj.address);
+console.log('');
+
+console.log( obj['100'] );
+
+var add = 'address';
+console.log( obj[add] );
+
+
