0d7d3352c848d2257d5b3fc8e987a5dcd1601038
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-21 16:00:24 +0900

    .

---
+++ Chap03_Component/js/type.js
@@ -0,0 +1,30 @@
+
+// 기본형
+var age = 10;
+console.log( typeof(age) );
+console.log( age.constructor )
+console.log('');
+
+var num = new Number(20);
+console.log( typeof(num) );
+console.log( num.constructor );
+console.log('');
+
+var str = 'ABC';
+console.log( typeof(str) );
+console.log( str.constructor );
+console.log('');
+
+var func = function() {}
+console.log( typeof(func) );
+console.log( func.constructor );
+console.log('');
+
+var arr = [];
+var obj = {};
+console.log( typeof(arr) );
+console.log( arr.constructor );
+console.log('')
+
+console.log( typeof(obj) );
+console.log( obj.constructor);
