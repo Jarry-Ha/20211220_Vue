daa81c3ec7b89160a1bf0caa494db88326309216
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-24 09:43:19 +0900

    .

--- Chap09_Vuex/src/components/TodoTemplate.vue
+++ Chap09_Vuex/src/components/TodoTemplate.vue
@@ -3,19 +3,20 @@
     <div>
         <h3>Todo List</h3>
 
+        <TodoForm></TodoForm>
+        <br>
+        <TodoList></TodoList>
+        
     </div>
 </template>
 
 <script>
-    // const sampleTodos = [
-    //     { id: 1, text: '첫 번째 할 일', done: true },
-    //     { id: 2, text: '두 번째 할 일', done: false },
-    //     { id: 3, text: '세 번째 할 일', done: false },
-    // ];
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
 
-    export default {
-        
-    }
+export default {
+    components: {TodoForm, TodoList}    
+}
 </script>
 
 <style scoped>
