025e603c595f665a1652be2a9cabbb9b907e1948
Automata <gkwoduf08@naver.com>
2022-04-08 13:41:53 +0900

    Vue2 연습
    practice 추가
     - Vue3 Bootstrap은 에러
     - Vue2로 Bootstrap 설정 완료

---
+++ Chap00_TIL/practice/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <StudyTest msg="Welcome to Your Vue.js App">
+            This is App Content 01 
+    </StudyTest>
+  </div>
+</template>
+
+<script>
+import StudyTest from './components/StudyTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    StudyTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Chap00_TIL/practice/src/components/StudyTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>hi</h3>
+    <slot
+      >전달한 값이 없으면, 기본값으로 이 내용이 표시된다. (필수는 아님. 필요에
+      따라 설정)</slot
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
