5220c8b3b34658a331c6500fe311fa8e4cbaed8d
Automata <gkwoduf08@naver.com>
2022-06-21 18:54:58 +0900

    220621

---
+++ Chap00_TIL/vue-practice/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <OpenHeader></OpenHeader>
+    <br />
+    <AxiosTest></AxiosTest>
+    <br />
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import OpenHeader from "./components/OpenHeader.vue";
+import AxiosTest from "./components/AxiosTest.vue";
+
+export default {
+  name: "App",
+  components: {
+    OpenHeader,
+    AxiosTest,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Chap00_TIL/vue-practice/src/components/AxiosTest.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <button v-on:click="fetchData">get data</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    fetchData: function () {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users/")
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
